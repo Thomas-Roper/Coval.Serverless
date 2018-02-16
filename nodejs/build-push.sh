c87dbb1aa0edfb63c6436b16e759f6d18bdd8017
shannon code <shannoncode@shannons-MacBook-Pro-2.local>
2018-02-16 13:28:08 -0500

    updated environment

--- nodejs/build-push.sh
+++ nodejs/build-push.sh
@@ -1,4 +1,4 @@
-docker build . --tag us.gcr.io/marine-compass-175800/coval:v3 $1
-gcloud docker -- push us.gcr.io/marine-compass-175800/coval:v3
+docker build . --tag us.gcr.io/marine-compass-175800/coval:v5 $1
+gcloud docker -- push us.gcr.io/marine-compass-175800/coval:v5
 fission env delete --name covaljs
-fission env create --name covaljs --image us.gcr.io/marine-compass-175800/coval:v3
+fission env create --name covaljs --image us.gcr.io/marine-compass-175800/coval:v5
