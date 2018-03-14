902156e93227279dac90d9ef4de7afe67423623d
shannon code <shannoncode@shannons-MacBook-Pro-2.local>
2018-03-14 16:30:10 -0400

    re-org

--- utils/gke-env.sh
+++ utils/gke-env.sh
@@ -1,2 +1,3 @@
+export PATH=$PATH:`pwd`
 export FISSION_URL=http://$(kubectl --namespace fission get svc controller -o=jsonpath='{..ip}')
 export FISSION_ROUTER=$(kubectl --namespace fission get svc router -o=jsonpath='{..ip}')
